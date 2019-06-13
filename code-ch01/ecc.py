707667a95fd78b397571df551661e8e47f58a02f
Brittany Latawiec <BRLatawiec@gmail.com>
2019-06-13 14:16:40 -0700

    Chapter solutions to exercises 1-9

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -23,7 +23,9 @@ class FieldElement:
 
     def __ne__(self, other):
         # this should be the inverse of the == operator
-        raise NotImplementedError
+        if other is None:
+            return True
+        return self.num != other.num or self.prime != other.prime
 
     # tag::source2[]
     def __add__(self, other):
@@ -36,18 +38,21 @@ class FieldElement:
     def __sub__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot subtract two numbers in different Fields')
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
+    
 
     def __mul__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot multiply two numbers in different Fields')
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
 
     # tag::source3[]
     def __pow__(self, exponent):
@@ -58,13 +63,14 @@ class FieldElement:
 
     def __truediv__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot divide two numbers in different Fields')
+            raise TypeError('Cannot divide two numbers in different Fields') 
+        num = (self.num * pow(other.num,self.prime-2,self.prime) % self.prime)
+        return self.__class__(num, self.prime) 
         # use fermat's little theorem:
         # self.num**(p-1) % p == 1
         # this means:
         # 1/n == pow(n, p-2, p)
         # We return an element of the same class
-        raise NotImplementedError
 
 
 class FieldElementTest(TestCase):
